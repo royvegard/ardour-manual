c2c4b0fd8e9c223699d1c927aea77028e041b71e
Carl Hetherington <cth@carlh.net>
2012-05-02 01:47:51 +0100

    Minor tidy-ups; move tracks/busses chapter up.

--- make.sh
+++ make.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+if [ `which inkscape`g == "g" ]; then
+    echo "You must have inkscape installed to build this manual"
+    exit 1
+fi
+
+if [ `which pdflatex`g == "g" ]; then
+    echo "You must have pdflatex installed to build this manual"
+    exit 1
+fi
+
+if [ `which makeindex`g == "g" ]; then
+    echo "You must have makeindex installed to build this manual"
+    exit 1
+fi
+
 for f in diagrams/*.svg; do
     g=`echo $f | sed -e "s/.svg/.pdf/"`
+    echo "Convert $f to PDF"
     inkscape -z -f $f --export-pdf $g --export-area-drawing
 done
 
 for f in graphics/*.svg; do
     g=`echo $f | sed -e "s/.svg/.pdf/"`
+    echo "Convert $f to PDF"
     inkscape -z -f $f --export-pdf $g --export-area-drawing
 done
 
